6a67e7e0ebaa10541a17ac3c16789b4a2495df84
Greg Stimson <stimsong@yahoo.com>
2018-04-18 20:23:21 +0000

    automatically backed up by learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,54 @@
+class MusicLibraryController
+  attr_accessor :library
+
+  def initialize(path = "./db/mp3s")
+    @path = path
+    library = MusicImporter.new(path)
+    library.import
+  end
+
+  def call
+    action = nil
+    while action != "exit" do
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      action = gets
+    end
+  end
+
+  def list_songs
+    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
+  end
+
+  def list_artists
+    Artist.all.sort{ |a, b| a.name <=> b.name}.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.name}"
+    end
+  end
+
+  def list_genres
+    Genre.all.sort{ |a, b| a.name <=> b.name}.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+
+    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      puts "#{i}. #{s.name} - #{s.genre.name}"
+    end
+
+  end
+
+end
