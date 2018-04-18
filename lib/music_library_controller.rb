77bd312c33cb868bb629f6b61c3c6cd7c848599e
Greg Stimson <stimsong@yahoo.com>
2018-04-18 21:09:40 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,8 +8,8 @@ class MusicLibraryController
   end
 
   def call
-    action = nil
-    while action != "exit" do
+    input = ""
+    while input != "exit" do
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -19,7 +19,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      action = gets
+      input = gets.strip
     end
   end
 
