60d24c956cb17dbf9e6f7a395e4808de8c06f928
Greg Stimson <stimsong@yahoo.com>
2018-04-18 20:44:24 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -58,7 +58,7 @@ class MusicLibraryController
 
     if genre = Genre.find_by_name(input)
       genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
-        puts "#{i}. #{a.song.name} - #{a.genre.name}"
+        puts "#{i}. #{a.songs.name} - #{a.genre.name}"
       end
     end
   end
